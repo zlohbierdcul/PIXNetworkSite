e57cdd2d4cd8c521e13799c9250110eb96bf2464
Luc Dreibholz <luc.dreibholz@gmx.net>
2024-01-24 04:39:42 +0100

    changed path in api controller

--- frontend/src/controller/API.js
+++ frontend/src/controller/API.js
@@ -1,22 +1,24 @@
 export const fetchAppData = async (setAppData, setLoading) => {
-    const res = await fetch('http://localhost:3001/api/getAppData', {
-        method: 'GET',
-    }).then(res => res.json())
-    setAppData(res)
-    setLoading(false)
+    const res = await fetch(
+        import.meta.env.VITE_BACKEND_URL + '/api/getAppData',
+        {
+            method: 'GET',
+        }
+    ).then((res) => res.json());
+    setAppData(res);
+    setLoading(false);
 };
-
 
 export const updateAppData = async (newAppData) => {
     const data = {
         appData: newAppData,
-    }
-    fetch("http://localhost:3001/api/updateAppData", {
-        method: "POST",
+    };
+    fetch(import.meta.env.VITE_BACKEND_URL + '/api/updateAppData', {
+        method: 'POST',
         headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
         },
         body: JSON.stringify(data),
-    })
-}
+    });
+};
